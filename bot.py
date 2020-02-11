937e0240b6ced9f2504ea5f1d70d2a455ded6ee6
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-02-11 22:55:52 +0900

    Bug fix

--- bot.py
+++ bot.py
@@ -29,4 +29,14 @@ async def on_ready():
     print('Logged in as{}'.format(client.user))
     await client.change_presence(status=eval('discord.Status.{}'.format(status)), activity=discord.Game(activity)) # presence 를 설정 데이터로 적용합니다.
 
+@client.event
+async def on_message(message):
+    if message.author.bot or message.author == client.user:
+        return
+    
+    if message.content.startswith(prefix + ' 도움'):
+        embed=discord.Embed(title="연어봇 - 도움", description="추가될 예정입니다.", color=0xff8040)
+        await message.channel.send(embed=embed)
+        return
+
 client.run(token)
