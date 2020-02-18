26719afb94c1dc2fd4b6215c1c8a8b0e49e3825d
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-02-18 13:06:15 +0900

    Coding

--- bot.py
+++ bot.py
@@ -72,6 +72,10 @@ async def on_message(message):
     if message.channel.type == discord.ChannelType.group or message.channel.type == discord.ChannelType.private: serverid_or_type = message.channel.type
     else: serverid_or_type = message.guild.id
 
+    def checkmsg(m):
+        return m.channel == message.channel and m.author == message.author
+
+    # 유저 등록
     if message.content == prefix + '등록':
         if str(message.author.id) in userdata:
             await message.channel.send(f'<@{message.author.id}> 이미 등록되어 있어요!')
@@ -82,23 +86,32 @@ async def on_message(message):
             embed.add_field(name='ㅤ', value='[이용약관](https://www.infiniteteam.me/tos)\n', inline=True)
             embed.add_field(name='ㅤ', value='[개인정보 취급방침](https://www.infiniteteam.me/privacy)\n', inline=True)
             await message.channel.send(embed=embed)
-            log(message.author.id, message.channel.id, message.content, '[이용약관 및 개인정보 취급방침의 동의]', fwhere_server=serverid_or_type)
-            def check(m):
-                return m.channel == message.channel and m.author == message.author
+            log(message.author.id, message.channel.id, message.content, '[이용약관 및 개인정보 취급방침의 동의]', fwhere_server=serverid_or_type) 
             try:
-                msg = await client.wait_for(('message'), timeout=20.0, check=check)
+                msg = await client.wait_for('message', timeout=20.0, check=checkmsg)
             except asyncio.TimeoutError:
                 await message.channel.send('시간이 초과되었습니다.')
             else:
                 if msg.content == '동의':
                     today = datetime.datetime.today()
-                    userdata[str(message.author.id)] = {'level': 1, 'type': 'User', 'date': f'{today.year}-{today.month}-{today.day}'}
-                    savedata(user=userdata)
-                    await message.channel.send(f'등록되었습니다. `{prefix}도움` 을 입력해서 전체 명령어를 볼 수 있습니다.')
-                    log(message.author.id, message.channel.id, msg.content, '[약관]', fwhere_server=serverid_or_type)
+                    tyear, tmonth, tday = str(today.year), str(today.month), str(today.day)
+                    if len(tyear) == 1:
+                        tyear = f'0{tyear}'
+                    if len(tmonth) == 1:
+                        tmonth = f'0{tmonth}'
+                    if len(tday) == 1:
+                        tyear = f'0{tday}'
+
+                    if str(message.author.id) in userdata: # 중복 등록 방지
+                        await message.channel.send(f'오류! 이미 등록된 사용자입니다! `{prefix}등록` 명령을 여러 번 입력한 것으로 추측됩니다.')
+                        log(message.author.id, message.channel.id, msg.content, '[오류: 중복 등록 감지 및 등록 취소]', fwhere_server=serverid_or_type)
+                    else:
+                        userdata[str(message.author.id)] = {'level': 1, 'type': 'User', 'date': f'{tyear}.{tmonth}.{tday}'}
+                        savedata(user=userdata)
+                        await message.channel.send(f'등록되었습니다. `{prefix}도움` 을 입력해서 전체 명령어를 볼 수 있습니다.')
+                        log(message.author.id, message.channel.id, msg.content, '[등록 완료]', fwhere_server=serverid_or_type)
                 else:
                     await message.channel.send('취소되었습니다. 다시 시도해 주세요.')
-                
         return
 
     # 등록되지 않은 유저일 경우
@@ -110,6 +123,25 @@ async def on_message(message):
         log(message.author.id, message.channel.id, message.content, '[미등록 사용자]', fwhere_server=serverid_or_type)
         return
 
+    elif message.content == prefix + '탈퇴':
+        await message.channel.send(f'<@{message.author.id}>')
+        embed = discord.Embed(title=f'{botname} 탈퇴',
+        description='''**연어봇 이용약관 및 개인정보 취급방침 동의를 철회하고, 연어봇을 탈퇴하게 됩니다.
+        이 경우 _사용자님의 모든 데이터(개인정보 취급방침을 참조하십시오)_가 연어봇에서 삭제되며, __되돌릴 수 없습니다.__**
+        계속하시려면 ``탈퇴``를 입력하십시오.''', color=color['warn'], timestamp=datetime.datetime.utcnow())
+        embed.add_field(name='ㅤ', value='[이용약관](https://www.infiniteteam.me/tos)\n', inline=True)
+        embed.add_field(name='ㅤ', value='[개인정보 취급방침](https://www.infiniteteam.me/privacy)\n', inline=True)
+        await message.channel.send(embed=embed)
+        log(message.author.id, message.channel.id, message.content, '[사용자 탈퇴]', fwhere_server=serverid_or_type)
+        try:
+            msg = await client.wait_for('message', timeout=20.0, check=checkmsg)
+        except asyncio.TimeoutError:
+            await message.channel.send('시간이 초과되었습니다.')
+        else:
+            if msg.content == '탈퇴':
+                pass
+        return
+
     # 수신 위치가 서버이고 미등록 서버인 경우. 그리고 설치 명령 실행 시에는 이 알림이 발신되지 않음.
     if message.content.startswith(prefix) and type(serverid_or_type) == int and not str(message.guild.id) in serverdata:
         embed=discord.Embed(title='⏰미등록 서버', description=f'**등록되어 있지 않은 서버입니다!**\n`{prefix}설치`명령을 입력해서, 봇 설정을 완료해주세요.', color=color['error'], timestamp=datetime.datetime.utcnow())
@@ -118,10 +150,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         log(message.author.id, message.channel.id, message.content, '[미등록 서버]', fwhere_server=serverid_or_type)
         return
-
-    elif message.content == prefix + '탈퇴':
-        pass
-
+    
     elif message.content == prefix + '도움':
         embed=discord.Embed(title='전체 명령어 목록', color=color['default'], timestamp=datetime.datetime.utcnow())
         embed.add_field(name='**연어봇**', value=f'**`{prefix}정보`**: {botname}의 버전, 개발자 정보 등 확인', inline=True)
@@ -198,10 +227,12 @@ def log(fwho, fwhere_channel, freceived, fsent, fetc=None, fwhere_server=None):
     now = datetime.datetime.today()
     fwhen = f'{now.year}-{now.month}-{now.day} {now.hour}:{now.minute}:{now.second}:{now.microsecond}'
     if fwhere_server == discord.ChannelType.group:
-        print(f'[{fwhen}] [ChannelType:] Group, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV]: {freceived}, [Sent]: {fsent}, [etc]: {fetc}')
+        logline = f'[{fwhen}] [ChannelType:] Group, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV]: {freceived}, [Sent]: {fsent}, [etc]: {fetc}'
     elif fwhere_server == discord.ChannelType.private:
-        print(f'[{fwhen}] [ChannelType:] DM, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV]: {freceived}, [Sent]: {fsent}, [etc]: {fetc}')
+        logline = f'[{fwhen}] [ChannelType:] DM, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV]: {freceived}, [Sent]: {fsent}, [etc]: {fetc}'
     else:
-        print(f'[{fwhen}] [ServerID:] {fwhere_server}, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV:] {freceived}, [Sent:] {fsent}, [etc:] {fetc}')
+        logline = f'[{fwhen}] [ServerID:] {fwhere_server}, [ChannelID:] {fwhere_channel}, [Author:] {fwho}, [RCV:] {freceived}, [Sent:] {fsent}, [etc:] {fetc}'
+    print(logline)
+    
 
 client.run(token)
