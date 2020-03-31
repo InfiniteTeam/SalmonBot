4ffa11b6adf8e99fcc94f474114925e87228068a
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-03-31 21:00:06 +0900

    update

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ async def on_ready():
         logger.warning(f'BETA MODE ENABLED.')
         pulse.send_pulse.start(client=client, user='salmonbot-beta', token=token)
     else:
-        pulse.send_pulse.start(client=client, user='salmonbot', tokenfile=token_file)
+        pulse.send_pulse.start(client=client, user='salmonbot', token=token)
     secloop.start()
     dbrecon.start()
     activityLoop.start()
--- iftext/pulse.py
+++ iftext/pulse.py
@@ -6,24 +6,13 @@ import traceback
 import requests
 
 @tasks.loop(seconds=5)
-async def send_pulse(client, user, token=None, tokenfile=None):
-    try:
-        if token != None:
-            headers = {
-                'IMS-User': user,
-                'IMS-Token': token
-            }
-        elif tokenfile != None:
-
-            headers = {
-                'IMS-User': user,
-                'IMS-Token': tokenfile.read()
-            }
+async def send_pulse(client, user, token):
+    try:       
         dataset = {
             'client.users/len': len(client.users),
             'client.guilds/len': len(client.guilds),
             'client.latency': client.latency
             }
-        resp = requests.post('http://arpa.kro.kr:5000/ims/dataset', json=dataset, headers=headers)
+        resp = requests.post('http://arpa.kro.kr:5000/ims/dataset', json=dataset, auth=(user, token))
     except:
         traceback.print_exc()
