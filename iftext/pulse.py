ddb6351295cc2a33d392da0c63db0c68fdaf8287
ArpaAP <arpa1705@gmail.com>
2020-04-05 18:18:57 +0900

    update

--- iftext/pulse.py
+++ iftext/pulse.py
@@ -19,14 +19,18 @@ async def send_pulse(client, user, token, host, version=None):
         for g in client.guilds:
             guildids.append(g.id)
         dataset = {
-            'version': version,
-            'last-alive-time': datetime.datetime.now().strftime('%Y-%m-%d %X'),
-            'client.users/len': len(client.users),
-            'client.guilds/len': len(client.guilds),
-            'client.users;ids': userids,
-            'client.guilds;ids': guildids,
-            'client.latency': client.latency
+            'public': {
+                'version': version,
+                'last-alive-time': datetime.datetime.now().strftime('%Y-%m-%d %X'),
+                'client.users/len': len(client.users),
+                'client.guilds/len': len(client.guilds),
+                'client.latency': client.latency
+            },
+            'private': {
+                'client.users;ids': userids,
+                'client.guilds;ids': guildids,
             }
+        }
         resp = requests.post(f'http://{host}/ims/dataset', json=dataset, headers=headers)
     except:
         traceback.print_exc()
